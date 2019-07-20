11c5f14f1f41e92b2bd6381ca566d4683bf67ab4
Ram496 <pasam.ramalingaiah@gmail.com>
2019-07-20 14:41:05 +0530

    commited changes

---
+++ src/main/java/db/springboot/camel/amq/springbootcamelamqdemo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package db.springboot.camel.amq.springbootcamelamqdemo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootcamelamqdemoApplication.class);
+	}
+
+}
---
+++ src/main/java/db/springboot/camel/amq/springbootcamelamqdemo/SpringbootcamelamqdemoApplication.java
@@ -0,0 +1,13 @@
+package db.springboot.camel.amq.springbootcamelamqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootcamelamqdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootcamelamqdemoApplication.class, args);
+	}
+
+}
